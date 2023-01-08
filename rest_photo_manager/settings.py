692afff5dc367a70053d00e9d0de2dc40fe70576
Fiskless <fiskless@gmail.com>
2023-01-08 16:42:17 +0500

    change type variable

--- rest_photo_manager/settings.py
+++ rest_photo_manager/settings.py
@@ -27,7 +27,7 @@ env.read_env()
 SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.str('DEBUG', True)
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1','localhost','0.0.0.0'])
 
